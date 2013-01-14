a4832e336c7c823d8014c2bf1b0e8c4e4d8800ac
cosminfirta <firtacosmin@yahoo.com>
2013-01-14 17:23:01 +0200

    added trajectory page added no of motion segments in settings

---
+++ src/com/syntese/graphics/wizard/pages/TrajectoriesPage.java
@@ -0,0 +1,87 @@
+package com.syntese.graphics.wizard.pages;
+
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.syntese.settings.SettingsFactory;
+
+public class TrajectoriesPage extends JPanel {
+	/****************
+	 * PROPERTIES
+	 * *************/
+	/*settings name*/
+	private static final String MAX_MOTION_SEGMENTS_SETTING_NAME = "MaxMotionSegmets";
+	
+	/*TEXTS*/
+	private static final String SECTIONS_TAG_NAME = "";
+	private static final String PHI_TAG_NAME = "";
+	private static final String MM_PSI_TAG_NAME = "";
+	private static final String NO_MOTION_SEG_TAG_NAME = "";
+	private static final String SUM_PHI_TAG_NAME = "";
+	private static final String SUM_MM_PSI_TAG_NAME = "";
+	
+	
+	/*GUI components*/
+	private JComboBox[] _setions;
+	private JTextField[] _phi;
+	private JTextField[] _mmPsi;
+	private JComboBox _noMotionSegments;
+	private JTextField _sumPhi;
+	private JTextField _sumMmPsi;
+	
+	private int _maxMotionSegments;
+	
+	
+	/****************
+	 * METHODS
+	 * *************/
+	
+	/*
+	 * PUBLIC
+	 * */
+	public TrajectoriesPage(){
+		super();
+		
+		try{
+			/*first try to get the current setting*/
+			_maxMotionSegments = Integer.parseInt(SettingsFactory.getInstance().getCurrentSetting(MAX_MOTION_SEGMENTS_SETTING_NAME));
+		}catch(NumberFormatException ex){
+			try {
+				/*if current setting is not correct then get the default setting*/
+				_maxMotionSegments = Integer.parseInt(SettingsFactory.getInstance().getDefaultSetting(MAX_MOTION_SEGMENTS_SETTING_NAME));
+			}catch(NumberFormatException ex2){
+				/*if the default setting is also incorrect then set the maximum number of segments to 12*/
+				_maxMotionSegments = 12;
+			}
+		}
+		
+		inializeGuiComponents();
+		addGuiComponents();
+	}
+
+
+	
+	/*
+	 * PRIVATE
+	 * */
+	private void inializeGuiComponents() {
+		
+		/*initializing the arrays*/
+		_setions = new JComboBox[_maxMotionSegments];
+		_mmPsi = new JTextField[_maxMotionSegments];
+		_phi = new JTextField[_maxMotionSegments];
+		
+		_noMotionSegments = new JComboBox();
+		
+		_sumPhi = new JTextField();
+		
+		_sumMmPsi = new JTextField();
+	}
+
+	private void addGuiComponents() {
+		
+	}
+}

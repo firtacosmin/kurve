c07b17e7e850f57c0a96e07fdca5abb30543befc
Cosmin <Cosmin@CosminPC>
2013-03-05 00:04:10 +0200

    added the sign definition tab to the last page tab class.

--- src/com/syntese/graphics/wizard/pages/LastPageTab.java
+++ src/com/syntese/graphics/wizard/pages/LastPageTab.java
@@ -6,10 +6,26 @@ import java.awt.Dimension;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
+import com.syntese.graphics.wizard.pages.lastPage.GeometryDataTab;
+import com.syntese.graphics.wizard.pages.lastPage.SignDefinitionTab;
+import com.syntese.language.LanguageFactory;
+
 public class LastPageTab extends JPanel{
 
-	private GeometryDataPage _geomTab; 
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final String GEOMETRY_TAB_TITLE = "Wizard_GeomData_TabTitle";
+	private static final String SIGN_DEFINITION_TAB_TITLE = "Wizard_SignDef_tabTitle";
+	
+	private GeometryDataTab _geomTab; 
+	private SignDefinitionTab _signTab;
 	private JTabbedPane _theTabbedPane;
+	
 	
 	public LastPageTab(int camType, int camProfile, int downStream){
 		super();
@@ -17,8 +33,10 @@ public class LastPageTab extends JPanel{
 		setLayout(new BorderLayout());
 		
 		_theTabbedPane = new JTabbedPane();
-		_geomTab = new GeometryDataPage(camType, camProfile, downStream);
-		_theTabbedPane.addTab("Geometry", _geomTab);
+		_geomTab = new GeometryDataTab(camType, camProfile, downStream);
+		_signTab = new SignDefinitionTab();
+		_theTabbedPane.addTab(LanguageFactory.getInstance().getExpresion(GEOMETRY_TAB_TITLE), _geomTab);
+		_theTabbedPane.addTab(LanguageFactory.getInstance().getExpresion(SIGN_DEFINITION_TAB_TITLE), _signTab);
 		
 		add(_theTabbedPane, BorderLayout.CENTER);
 		Dimension d = _geomTab.getSize();

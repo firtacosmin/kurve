d308a765a6e5bd5fd8ba974a0032eb57577d2410
Cosmin <Cosmin@CosminPC>
2013-03-06 23:54:58 +0200

    added the motion tab to the last page

--- src/com/syntese/graphics/wizard/pages/LastPageTab.java
+++ src/com/syntese/graphics/wizard/pages/LastPageTab.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import com.syntese.graphics.wizard.pages.lastPage.GeometryDataTab;
+import com.syntese.graphics.wizard.pages.lastPage.MotionTab;
 import com.syntese.graphics.wizard.pages.lastPage.SignDefinitionTab;
 import com.syntese.language.LanguageFactory;
 
@@ -21,10 +22,12 @@ public class LastPageTab extends WizardPage{
 	
 	private static final String GEOMETRY_TAB_TITLE = "Wizard_GeomData_TabTitle";
 	private static final String SIGN_DEFINITION_TAB_TITLE = "Wizard_SignDef_tabTitle";
+	private static final String MOTION_TAB_TITLE = "Wizard_Motion_TabTitle";
 	
 	private GeometryDataTab _geomTab; 
 	private SignDefinitionTab _signTab;
 	private JTabbedPane _theTabbedPane;
+	private MotionTab _theMotionTab;
 	
 	
 	public LastPageTab(int camType, int camProfile, int downStream){
@@ -35,8 +38,10 @@ public class LastPageTab extends WizardPage{
 		_theTabbedPane = new JTabbedPane();
 		_geomTab = new GeometryDataTab(camType, camProfile, downStream);
 		_signTab = new SignDefinitionTab();
+		_theMotionTab = new MotionTab();
 		_theTabbedPane.addTab(LanguageFactory.getInstance().getExpresion(GEOMETRY_TAB_TITLE), _geomTab);
 		_theTabbedPane.addTab(LanguageFactory.getInstance().getExpresion(SIGN_DEFINITION_TAB_TITLE), _signTab);
+		_theTabbedPane.addTab(LanguageFactory.getInstance().getExpresion(MOTION_TAB_TITLE), _theMotionTab);
 		
 		add(_theTabbedPane, BorderLayout.CENTER);
 		Dimension d = _geomTab.getSize();

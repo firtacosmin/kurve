f4c6e8ccccb9a411dc09b6d6086c45a6420b3175
Cosmin <Cosmin@CosminPC>
2013-03-06 23:57:07 +0200

    created segment package for containing the data related to the segments. create the segmentFactory class to get the list of all the segments(for now)

---
+++ src/com/syntese/segment/SegmentFactory.java
@@ -0,0 +1,20 @@
+package com.syntese.segment;
+
+import java.util.ArrayList;
+
+public class SegmentFactory {
+
+	/**
+	 * Name: getSegmentTitleList
+	 * Args: @return
+	 * Return: ArrayList<String>
+	 * Desc: returns a list with the titles of all the segments. 
+	 */
+	public static ArrayList<String> getSegmentTitleList(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("one");
+		list.add("doi");
+		return list;
+	}
+	
+}
